512ab9e5aaef09758cd7d1436088c8176f87eed7
Abo-Elhassan <107323255+Abo-Elhassan@users.noreply.github.com>
2023-02-06 07:48:59 +0200

    ios deployment

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -21,16 +21,16 @@ class SplashController extends GetxController {
     super.onReady();
   }
 
-  void initApplication() {
+  void initApplication() async {
     Future.delayed(Duration(seconds: 4), () async {
       AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
       if (Platform.isAndroid) {
         FlutterJailbreakDetection.developerMode.then((value) {
-          // if (value || !(androidInfo.isPhysicalDevice)) {
-          //   Get.offAllNamed(Routes.ERROR);
-          // } else {
-          Get.offAllNamed(Routes.LANDING);
-          // }
+          if (value || !(androidInfo.isPhysicalDevice)) {
+            Get.offAllNamed(Routes.ERROR);
+          } else {
+            Get.offAllNamed(Routes.LANDING);
+          }
         });
       } else if (Platform.isIOS) {
         IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
@@ -38,45 +38,47 @@ class SplashController extends GetxController {
         FlutterJailbreakDetection.jailbroken.then((value) {
           if (value == true || !iosInfo.isPhysicalDevice) {
             Get.offNamed(Routes.ERROR);
+          } else {
+            Get.offAllNamed(Routes.LANDING);
           }
         });
       }
 
-      // RootCheck.isRooted.then((value) {
-      //   if (value != null && value) {
-      //     exit(0);
-      //   }
-      // });
+      RootCheck.isRooted.then((value) {
+        if (value != null && value) {
+          exit(0);
+        }
+      });
 
-      // RootCheck.isRootedWithBusyBoxCheck.then((value) {
-      //   if (value != null && value) {
-      //     exit(0);
-      //   }
-      // });
+      RootCheck.isRootedWithBusyBoxCheck.then((value) {
+        if (value != null && value) {
+          exit(0);
+        }
+      });
 
-      // RootCheck.checkForRootNative.then((value) {
-      //   if (value != null && value) {
-      //     exit(0);
-      //   }
-      // });
+      RootCheck.checkForRootNative.then((value) {
+        if (value != null && value) {
+          exit(0);
+        }
+      });
 
-      // RootCheck.detectPotentiallyDangerousApps.then((value) {
-      //   if (value != null && value) {
-      //     exit(0);
-      //   }
-      // });
+      RootCheck.detectPotentiallyDangerousApps.then((value) {
+        if (value != null && value) {
+          exit(0);
+        }
+      });
 
-      // RootCheck.detectTestKeys.then((value) {
-      //   if (value != null && value) {
-      //     exit(0);
-      //   }
-      // });
+      RootCheck.detectTestKeys.then((value) {
+        if (value != null && value) {
+          exit(0);
+        }
+      });
 
-      // RootCheck.checkForDangerousProps.then((value) {
-      //   if (value != null && value) {
-      //     exit(0);
-      //   }
-      // });
+      RootCheck.checkForDangerousProps.then((value) {
+        if (value != null && value) {
+          exit(0);
+        }
+      });
     });
   }
 }
