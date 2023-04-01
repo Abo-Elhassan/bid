54df1eafce1a36f4cfeabe2b35d1ff2159e2b9c5
Abo-Elhassan <107323255+Abo-Elhassan@users.noreply.github.com>
2023-04-01 13:00:21 +0200

    test vs

--- lib/app/core/utilities/charts/map_chart.dart
+++ lib/app/core/utilities/charts/map_chart.dart
@@ -2,7 +2,6 @@ import 'package:bid_app/app/core/values/app_assets.dart';
 import 'package:bid_app/app/data/models/responses/filter_data_response.dart';
 import 'package:bid_app/app/core/utilities/helpers.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'dart:math' as math;
 import 'package:syncfusion_flutter_maps/maps.dart';
 
